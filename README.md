6e15bb609a60169e4410cee268702acb2448ce20
Vladimir Mishin <drli396@yandex.ru>
2021-09-19 10:27:41 +0300

    clearup main

--- README.md
+++
@@ -1,5 +0,0 @@
-# movies-explorer-api
-## Дипломная работа - бэкенд
-
-### Ссылка на сервер https://api.movies.mishin.nomoredomains.club
-### IP сервера 84.201.178.224
--- errors/authorization-err.js
+++
@@ -1,8 +0,0 @@
-class AuthorizationErr extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-  }
-}
-
-module.exports = AuthorizationErr;
