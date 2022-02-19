23b6b625771bc32d8aef8d897a2aa707107b17d4
Vladimir Mishin <drli396@yandex.ru>
2022-02-19 08:53:49 +0300

    cors: headers

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,14 +3,13 @@ const allowedCorse = ['http://movies.mishin.nomoredomains.club',
   'http://movies-frontend-git-main-vladimirmishin1976.vercel.app',
   'http://movies-frontend-vladimirmishin1976.vercel.app/',
   'http://localhost:3000',
-  '*',
-];
+  '*'];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+  // const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCorse.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -19,7 +18,7 @@ module.exports = (req, res, next) => {
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
     return res.end();
   }
   return next();
